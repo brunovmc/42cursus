146665c447123b6e5b1d64c3e5cf6828073f9fa6
brunovmc <bruno.vidigal.coriolano@gmail.com>
2020-09-25 02:37:46 +0200

    primeiro commit cub3d

---
+++ cub3d/include/cub3d.h
@@ -0,0 +1,13 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "mlx.h"
+# include <math.h>
+
+# define NUM_COLS 480
+
+# define NUM_ROWS 320
+
+# define PI 3.1416
+
+#endif
---
+++ cub3d/srcs/main.c
@@ -0,0 +1,66 @@
+#include "cub3d.h"
+#include <stdio.h>
+
+void 	line(void *mlx, void *window, int x, int y, int size)
+{
+	int i;
+
+	i = 0;
+	while(i <= size)
+	{
+		mlx_pixel_put(mlx, window, x + i, y, 0255255255);
+		i++;
+	}
+}
+
+void 	square(void *mlx, void *window, int x, int y, int size)
+{
+	int i;
+
+	i= 0;
+	while (i <= size)
+	{
+		mlx_pixel_put(mlx, window, x + i, y, 0777777777);
+		mlx_pixel_put(mlx, window, x, y + i, 0777777777);
+		mlx_pixel_put(mlx, window, x + size, y + i, 0777777777);
+		mlx_pixel_put(mlx, window, x + i, y + size, 0777777777);
+		i++;
+	}
+	
+}
+
+void	circle(void *mlx, void *window, int x, int y, int raio)
+{
+	float	ang;
+	float	addang;
+	int	new_x;
+	int	new_y;
+
+	ang = 0.0;
+
+	while (ang < 2 * PI)
+	{
+		new_x = x + (raio * cos(ang));
+		new_y = y + (raio * sin(ang));
+		mlx_pixel_put(mlx, window, x + new_x, y + new_y, 0660000);
+		ang += addang;
+	}
+}
+
+int	main(void)
+{
+	void *mlx;
+	void *window;
+	
+	
+	mlx = mlx_init();
+	window = mlx_new_window(mlx, NUM_COLS, NUM_ROWS, "Janela Janelinha!");
+	mlx_string_put(mlx, window, 20,  20, 0500000, "teste");
+	mlx_pixel_put(mlx, window, 300, 300, 0707070);
+	line(mlx, window, 30, 30, 10000);
+	square(mlx, window, 20, 20, 20);
+	circle(mlx, window, 50, 50, 50);
+	mlx_loop(mlx);
+
+	return (0);
+}
